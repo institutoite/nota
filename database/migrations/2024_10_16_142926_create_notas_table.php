b2778412686d15553190655dc5782db902ad93d8
institutoite <informaciones.ite@gmail.com>
2024-10-16 10:35:54 -0400

    first commit

---
+++ database/migrations/2024_10_16_142926_create_notas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->$table->tinyInteger('tremestre1');
+            $table->$table->tinyInteger('tremestre2');
+            $table->$table->tinyInteger('tremestre3');
+            $table->$table->tinyInteger('materia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notas');
+    }
+};
