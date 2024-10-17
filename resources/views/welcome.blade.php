8079e04d978e2114d9ade222a34bdb6ecb70ca3c
institutoite <informaciones.ite@gmail.com>
2024-10-17 11:17:13 -0400

    mejorando iteizando el formulario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,17 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <div id="contenedor">
         <div id="central">
             <div id="login">
+                
                 <div class="titulo">
                     Bienvenido
                 </div>
-                <form id="loginform">
-                    <input type="text" name="trimestre1" placeholder="nota primer trimestre" required>
-                    <input type="text" name="trimestre2" placeholder="nota segundo trimestre" required>
+                <form>
+                    <input type="text" name="trimestre1" id="trimestre1" placeholder="nota primer trimestre" required>
+                    <input type="text" name="trimestre2" id="trimestre2" placeholder="nota segundo trimestre" required>
                     <select class="custom-select" id="materia_id" name="materia_id" required>
                         <option value="">Seleccione una materia</option>
                         @foreach($materias as $materia)
@@ -25,9 +27,9 @@
                     </select>
                     <input type="text" name="telefono" placeholder="telefono" required>
                     
-                    <input type="promediodeseado" placeholder="Promedio deseado" value="51" name="promediodeseado" required>
+                    <input type="number" id="promedio_minimo" placeholder="Promedio deseado" value="51" name="promedio_minimo"  required>
                     
-                    <button type="submit" title="Ingresar" name="Ingresar">Calcular</button>
+                    <button type="submit" title="Ingresar" id="calcular-btn" name="Ingresar">Calcular</button>
                 </form>
                 <div class="pie-form">
                     <a href="#">sigueme en tik tok</a>
@@ -37,7 +39,9 @@
     </div>
 
 <script>
-document.getElementById('calcular-btn').addEventListener('click', function() {
+document.getElementById('calcular-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("todo bien");
     // Obtener valores de los trimestres y el promedio mínimo
     var trimestre1 = parseFloat(document.getElementById('trimestre1').value) || 0;
     var trimestre2 = parseFloat(document.getElementById('trimestre2').value) || 0;
@@ -51,7 +55,8 @@ document.getElementById('calcular-btn').addEventListener('click', function() {
     trimestre3 = Math.max(trimestre3, 0);
 
     // Mostrar el valor calculado en el campo de trimestre3
-    document.getElementById('trimestre3').value = trimestre3.toFixed(2);
+    //document.getElementById('trimestre3').value = trimestre3.toFixed(2);
+    //document.getElementById('respuesta').innerHTML = "El promedio mínimo es: " + promedioMinimo + " y el valor necesario en el tercer trimestre es: " + trimestre3.toFixed(2);
 });
 </script>
 </body>
